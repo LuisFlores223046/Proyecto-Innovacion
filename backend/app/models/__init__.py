877b7c43d42ed8ba0907bc1a96808f9340b976e7
LuisFlores223046 <al223046@alumnos.uacj.mx>
2026-04-02 14:33:41 -0600

    Carga de Elementos restantes para leer espacios en front

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,23 @@
+from app.models.categoria import Categoria
+from app.models.edificio import Edificio
+from app.models.piso import Piso
+from app.models.espacio import Espacio
+from app.models.horario import Horario
+from app.models.contacto import Contacto
+from app.models.servicio_espacio import ServicioEspacio
+from app.models.foto_espacio import FotoEspacio
+from app.models.evento import Evento
+from app.models.administrador import Administrador
+
+__all__ = [
+    "Categoria",
+    "Edificio",
+    "Piso",
+    "Espacio",
+    "Horario",
+    "Contacto",
+    "ServicioEspacio",
+    "FotoEspacio",
+    "Evento",
+    "Administrador",
+]
